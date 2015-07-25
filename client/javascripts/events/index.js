80ed4a551c2cea9f3c9a70a1556368d9054e610c
samundrak <samundrak@yahoo.com>
2015-07-25 22:18:05 +0545

    Now status post and displaying has added

--- client/javascripts/events/index.js
+++ client/javascripts/events/index.js
@@ -1,71 +1,106 @@
+Template.header.events({
+	'click .logout': function(event) {
+		Meteor.logout(function(err) {
+			if (!err) {
+				Router.go('login');
+			} else {
+				msgBox(err.reason);
+			}
+		});
+	}
+});
+Template.login.events({
+	'submit .loginform': function(event) {
+		event.preventDefault();
+		var username = event.target.username.value;
+		var password = event.target.password.value;
+		Meteor.loginWithPassword(username, password, function(err) {
+			if (err) {
+				msgBox(err.reason)
+			} else {
+				Router.go('home')
+			}
+		});
+		// Meteor.call('loginUser',{email:username,password:password},function(error,result){
+		// 	console.log(error);
+		// 	console.log(result);
+		// })
+	}
+})
 
- Template.header.events({
- 	'click .logout': function(event) {
- 		Meteor.logout(function(err) {
- 			if (!err) {
- 				Router.go('login');
- 			} else {
- 				msgBox(err.reason);
- 			}
- 		});
+Template.register.events({
+	'submit .registerform': function(event) {
+		event.preventDefault();
+		var fields = ["username", "password", "email", "firstname", "lastname", "gender"];
+		var data = [];
+		fields.forEach(function(element, index) {
+			data.push(getInputData(event, element));
+		});
+		var insertData = {};
+		insertData.profile = {};
+		var validationPass;
+		data.some(function(element, index) {
+			if (!inputAuthorization(element)) {
+				alert(fields[index] + ' is empty');
+				validationPass = false;
+				return true;
+			} else {
+				if (fields[index] === 'username' || fields[index] === 'password' || fields[index] === 'email') {
+					insertData[fields[index]] = data[index];
+				} else {
+					insertData.profile[fields[index]] = data[index];
+				}
+
+			}
+			validationPass = true;
+
+		});
+		if (!validationPass) return false;
+		Meteor.call('registerUser', insertData, function(error, result) {
+			if (error) {
+				msgBox(error.reason)
+			} else {
+				msgBox('Register Successfull!')
+				Router.go('login');
+			}
+		});
+
+	}
+})
+
+Template.userfeed.events({
+	'click .postStatus': function(event, template) {
+		var val = $('.status').val();
+		$('.status').val('');
+		var data = {};
+		if (val.length > 0) {
+			data.status = val;
+
+			Meteor.call('statusPost', data, function(err, res) {
+				if (!err) {
+					console.log(res);
+					if (res) {
+						msgBox("Status has been posted")
+					} else {
+						msgBox("Status hasnt been posted")
+					}
+				} else {
+					msgBox(err.reason);
+				}
+			})
+		}
+	}
+})
+
+
+ function inputAuthorization(value) {
+ 	if (value && value.length > 0) {
+ 		return true;
+ 	} else {
+ 		return false;
  	}
- });
- Template.login.events({
- 	'submit .loginform': function(event) {
- 		event.preventDefault();
- 		var username = event.target.username.value;
- 		var password = event.target.password.value;
- 		Meteor.loginWithPassword(username, password, function(err) {
- 			if (err) {
- 				msgBox(err.reason)
- 			} else {
- 				Router.go('home')
- 			}
- 		});
- 		// Meteor.call('loginUser',{email:username,password:password},function(error,result){
- 		// 	console.log(error);
- 		// 	console.log(result);
- 		// })
- 	}
- })
+ }
 
- Template.register.events({
- 	'submit .registerform': function(event) {
- 		event.preventDefault();
- 		var fields = ["username", "password", "email", "firstname", "lastname", "gender"];
- 		var data = [];
- 		fields.forEach(function(element, index) {
- 			data.push(getInputData(event, element));
- 		});
- 		var insertData = {};
- 		insertData.profile = {};
- 		var validationPass;
- 		data.some(function(element, index) {
- 			if (!inputAuthorization(element)) {
- 				alert(fields[index] + ' is empty');
- 				validationPass = false;
- 				return true;
- 			} else {
- 				if (fields[index] === 'username' || fields[index] === 'password' || fields[index] === 'email') {
- 					insertData[fields[index]] = data[index];
- 				} else {
- 					insertData.profile[fields[index]] = data[index];
- 				}
-
- 			}
- 			validationPass = true;
-
- 		});
- 		if (!validationPass) return false;
- 		Meteor.call('registerUser', insertData, function(error, result) {
- 			if (error) {
- 				msgBox(error.reason)
- 			} else {
- 				msgBox('Login Successfull!')
- 				Router.go('login');
- 			}
- 		});
-
- 	}
- })
-
+ function getInputData(event, element) {
+ 	return event.target[element].value;
+ }
--- client/javascripts/helpers/index.js
+++ client/javascripts/helpers/index.js
@@ -1,16 +1,28 @@
 Template.userinfo.helpers({
-			basicinfo: function() {
-				console.log(Meteor.user())
-				return Meteor.user();
-			},
+	basicinfo: function() {
+		console.log(Meteor.user())
+		return Meteor.user();
+	},
 
-			email: function() {
-				return Meteor.user().emails[0].address;
-			},
+	email: function() {
+		return Meteor.user().emails[0].address;
+	},
 
-			gender: function() {
-				return (Meteor.user().profile.gender ) === 0 ? 'Male' : 'Female';
+	gender: function() {
+		return (Meteor.user().profile.gender) === 0 ? 'Male' : 'Female';
 
-
-			}
+	}
 });
+
+Template.header.helpers({
+	username: function() {
+		return Meteor.user().username;
+	}
+})
+Template.userfeed.helpers({
+	feeds: function() {
+		var posts =statusCl.find({created_by:Meteor.user()._id}, {sort: {created_at: -1}}).fetch();
+		console.log(posts);
+		return posts;
+	}
+})
